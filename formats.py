078dad7aab2ac6e5232bca3e4dacf83b5ae9e1dd
Balthazar Rouberol <rouberol.b@gmail.com>
2011-10-11 19:25:24 -0200

    Programming languages file definitions defined
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

---
+++ formats.py
@@ -0,0 +1,40 @@
+"""
+File containing a list of programming languages 
+and associated file extensions
+"""
+
+formats = {
+    'Bash'         : '.sh',
+    'C'            : '.c',
+    'CSS'          : '.css',
+    'C++'          : '.cpp',
+    'C#'           : '.cs',
+    'Cobol'        : '.cob',
+    'D'            : '.d',
+    'Eiffel'       : '.e',
+    'Fortran'      : '.f',
+    'HTML'         : '.html',
+    'Java'         : '.java',
+    'Javascript'   : '.js',
+    'Lisp'         : '.lisp',
+    'Lua'          : '.lua',
+    'Objective C'  : '.m',
+    'Pascal'       : '.p',
+    'Perl'         : '.pl',
+    'Php'          : '.php',
+    'Python'       : '.py',
+    'R'            : '.r',
+    'Ruby'         : '.rb',
+    'SQL'          : '.sql',
+    'Visual Basic' : '.bas',
+}
+    
+    
+    
+    
+    
+    
+    
+
+ 
+   
